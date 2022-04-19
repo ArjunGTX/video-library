902b3e6363dfe145a6aa6d78883294fac2dd57bb
ArjunGTX <arjunatpavilion@gmail.com>
2022-04-19 23:07:50 +0530

    categories

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -3,20 +3,22 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "Computer Programming",
-    description:
-      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
+    categoryName: "Eargasm",
+    category: "eargasm",
   },
   {
     _id: uuid(),
-    categoryName: "Frontend Development",
-    description:
-      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application",
+    categoryName: "Review",
+    category: "review",
   },
   {
     _id: uuid(),
-    categoryName: "Backend Development",
-    description:
-      "Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website.",
+    categoryName: "Adrenaline Rush",
+    category: "adrenaline-rush",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Upcoming Bikes",
+    category: "upcoming-bikes",
   },
 ];
